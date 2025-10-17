fc8d84ea8f5d9558faaedcf7a71ac6c1922da624
dvp-coms <dtv12345@hotmail.com>
2025-10-17 16:20:33 -0500

    Update light.rs supporting Some(false)

--- src/hass_mqtt/light.rs
+++ src/hass_mqtt/light.rs
@@ -80,7 +80,7 @@ impl EntityInstance for DeviceLight {
             Some(device_state) => {
                 log::trace!("LightConfig::notify_state: state is {device_state:?}");
 
-                if !device_state.online {
+                if !device_state.online ||  matches!(device_state.online, Some(false)) {
                     client
                         .publish_obj(&self.light.state_topic, &json!({"state":JsonValue::Null}))
                         .await;
