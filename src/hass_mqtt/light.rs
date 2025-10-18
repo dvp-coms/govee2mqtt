26030dc6aafd6d8fd37bf6d51ea9639d8ff55d06
dvp-coms <dtv12345@hotmail.com>
2025-10-18 07:56:32 -0500

    Update light.rs

--- src/hass_mqtt/light.rs
+++ src/hass_mqtt/light.rs
@@ -78,12 +78,12 @@ impl EntityInstance for DeviceLight {
 
         match device.device_state() {
             Some(device_state) => {
-                log::trace!("LightConfig::notify_state: state is {device_state:?}");
+                log::trace!("LightConfigDVP::notify_state: state is {device_state:?}");
                 //log::trace!("LightConfigDVP::notify_state: state.online is {device_state.online:?}");
                 
                 if matches!(device_state.online, Some(false)) {
                     client
-                        .publish_obj(&self.light.state_topic, &json!({"state":JsonValue::Null}))
+                        .publish_obj(&self.light.state_topic, &json!({"state":"unavailable"}))
                         .await;
                     
                     return Ok(());
